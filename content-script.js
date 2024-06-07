5517f229c8b4f8f3dfee79d017be664a79c452eb
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-06-07 19:10:34 -0400

    Create content-script.js

---
+++ content-script.js
@@ -0,0 +1,32 @@
+if (window == top) {
+  window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
+}
+
+trigger_key_acc = 71; // g key
+trigger_key_rej = 72; // g key
+function doKeyPress(e){
+  chrome.storage.local.get(["accept_hotkey"], function(items){
+    //  items = [ { "phasersTo": "awesome" } ]
+    trigger_key_acc = items["accept_hotkey"].charCodeAt();
+  }
+  chrome.storage.local.get(["reject_hotkey"], function(items){
+    //  items = [ { "phasersTo": "awesome" } ]
+    trigger_key_rej = items["reject_hotkey"].charCodeAt();
+  }
+	if (e.keyCode == trigger_key_acc){
+		//alert('Hi!')
+    console.log('accept button pressing...');
+    try {
+      document.querySelector("button:contains('accept')")[0].click();
+    } catch {
+      console.warn('accept button not found');
+    }
+	} else if (e.keyCode == trigger_key_rej){
+    console.log('reject button pressing...');
+    try {
+      document.querySelector("button:contains('reject')")[0].click();
+    } catch {
+      console.warn('reject button not found');
+    }
+  }
+}
