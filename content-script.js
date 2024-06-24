1acef79e90451d7378d4ac240a4bcbc1753e25ab
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2024-06-24 08:44:02 -0400

    test and bugfix

--- content-script.js
+++ content-script.js
@@ -5,18 +5,18 @@ if (window == top) {
 trigger_key_acc = 71; // g key
 trigger_key_rej = 72; // h key
 function doKeyPress(e){
-  chrome.storage.local.get(["accept_hotkey"], function(items){
-    //  items = [ { "phasersTo": "awesome" } ]
-    if (items["reject_hotkey"] !== undefined) {
-      trigger_key_acc = items["accept_hotkey"].charCodeAt();
-    }
-  });
-  chrome.storage.local.get(["reject_hotkey"], function(items){
-    //  items = [ { "phasersTo": "awesome" } ]
-    if (items["reject_hotkey"] !== undefined) {
-      trigger_key_rej = items["reject_hotkey"].charCodeAt();
-    }
-  });
+  // chrome.storage.local.get(["accept_hotkey"], function(items){
+  //   //  items = [ { "phasersTo": "awesome" } ]
+  //   if (items["reject_hotkey"] !== undefined) {
+  //     trigger_key_acc = items["accept_hotkey"].charCodeAt();
+  //   }
+  // });
+  // chrome.storage.local.get(["reject_hotkey"], function(items){
+  //   //  items = [ { "phasersTo": "awesome" } ]
+  //   if (items["reject_hotkey"] !== undefined) {
+  //     trigger_key_rej = items["reject_hotkey"].charCodeAt();
+  //   }
+  // });
 	if (e.keyCode == trigger_key_acc){
 		//alert('Hi!')
     console.log('accept button pressing...');
